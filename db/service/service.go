b4dfd8f6f2b6bc2534d4c6cdbc88be08ea0cac2b
Loris Dev <loriscn12@gmail.com>
2019-03-10 19:49:39 +0000

    Adding AddUser() RPC to Database service.

--- db/service/service.go
+++ db/service/service.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"time"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	
 	"log"
 	"fmt"
 	"net"
@@ -9,6 +14,7 @@ import (
 
 	dpb "golang-project/db/proto"
 	grpc "google.golang.org/grpc"
+	mongo "go.mongodb.org/mongo-driver/mongo"
 )
 
 var (
@@ -16,20 +22,36 @@ var (
 )
 
 type dbServer struct {
+	dbClient *mongo.Client
 }
 
 // Test returns a test response.
-func (s *dbServer) Test(ctx context.Context, req *dpb.TestRequest) (*dpb.TestResponse, error) {
-	return &dpb.TestResponse{Test: "response"}, nil
+func (s *dbServer) AddUser(ctx context.Context, req *dpb.AddUserRequest) (*dpb.AddUserResponse, error) {
+	err := s.dbClient.Ping(ctx, readpref.Primary())
+	if err != nil{
+		log.Fatalf("failed to connect to MongoDB client: %s", err)
+	}
+	collection := s.dbClient.Database("test").Collection("users")
+	_, err = collection.InsertOne(ctx, bson.M{"name": req.Name, "surname": req.Surname})
+	if err != nil{
+		log.Fatalf("failed to insert to MongoDB: %s", err)
+	}
+	return &dpb.AddUserResponse{Id: "test"}, nil
 }
 
 func main() {
 	flag.Parse()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel();
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
-	dpb.RegisterDatabaseServer(grpcServer, &dbServer{})
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatalf("failed to instantiate a new MongoDB client: %s", err)
+	}
+	dpb.RegisterDatabaseServer(grpcServer, &dbServer{dbClient: client})
 	grpcServer.Serve(lis)
 }
